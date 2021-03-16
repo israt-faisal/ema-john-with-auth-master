660236b74f2bd25474fadc5f75edd03436ff7a69
israt faisal <israt.nsu141@gmail.com>
2021-03-16 19:06:12 -0400

    ema-john-auth

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNTvE0lwmfkcH7lpAaDGiSymdOAxB09gI",
+  authDomain: "ema-john-simple-phero.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-phero-default-rtdb.firebaseio.com",
+  projectId: "ema-john-simple-phero",
+  storageBucket: "ema-john-simple-phero.appspot.com",
+  messagingSenderId: "658997159385",
+  appId: "1:658997159385:web:05fc5dcfb185d21baf65c8"
+};
+export default firebaseConfig;
